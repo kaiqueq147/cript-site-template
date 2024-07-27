1b3120ec719c1d57635377a10af4be4848589170
kaiqueq147 <kaiqueq147@gmail.com>
2024-07-27 09:37:17 -0300

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/images/123.png')",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        "50%": "50%",
+        16: "4rem",
+      },
+    },
+  },
+  plugins: [],
+};
